92d6bfd3b1b4be598798bf598d5437ff401ea74c
Pat Hornchaiya <pat.hornchaiya@hotmail.com>
2015-03-12 19:09:03 +0700

    Back Up before changing interface

--- public/js/main.js
+++ public/js/main.js
@@ -8,12 +8,8 @@ $(document).ready(function($) {
 		// console.log($("body").scrollTop());
 		if($("body").scrollTop() > 50) {
 			$(".navbar").addClass('highlight-nav');
-			$(".cart").addClass('left-border');
-			$(".cart").addClass('right-border');
 		} else {
 			$(".navbar").removeClass('highlight-nav');
-			$(".cart").removeClass('left-border');
-			$(".cart").removeClass('right-border');
 		}
 	});
 
