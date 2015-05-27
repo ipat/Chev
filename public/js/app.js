a56fabb8a0601cba23ee0a588b60bf43148b412a
iPat <pat.hornchaiya@hotmail.com>
2015-05-27 15:32:20 +0700

    -Insert Graph -Insert Or Yor

--- public/js/app.js
+++ public/js/app.js
@@ -278,157 +278,79 @@ chevApp.controller('homeController', function($scope, $rootScope){
 // Controller for Ingredients Page
 chevApp.controller('ingredientController', function($scope, $rootScope){
 	$rootScope.navbarClass = "text-dark";
-	// $(window).stellar();
-	// $.stellar({
-	//   horizontalScrolling: false
-	// });
-
-	// $("#click1").click(function(event) {
-	// 	$("#read1").slideToggle("slow");
-	// 	$(this).text(function(i, text){
-	// 		return text === "More Info v" ? "Less ^" : "More Info v";
-	// 	})
-	// });
-
-	// $("#click2").click(function(event) {
-	// 	$("#read2").slideToggle("slow");
-	// 	$(this).text(function(i, text){
-	// 		return text === "More Info v" ? "Less ^" : "More Info v";
-	// 	})
-	// });
-
-	// $("#click3").click(function(event) {
-	// 	$("#read3").slideToggle("slow");
-	// 	$(this).text(function(i, text){
-	// 		return text === "More Info v" ? "Less ^" : "More Info v";
-	// 	})
-	// });
-
-	// $("#click4").click(function(event) {
-	// 	$("#read4").slideToggle("slow");
-	// 	$(this).text(function(i, text){
-	// 		return text === "More Info v" ? "Less ^" : "More Info v";
-	// 	})
-	// });
-
-	// var data = {
-	//     labels: ["1000 mg of L-carnitine/day", "1000 mg of other low calorie diet group/day"],
-	//     datasets: [
-	//         {
-	//             label: "My First dataset",
-	//             fillColor: "rgba(220,220,220,0.5)",
-	//             strokeColor: "rgba(220,220,220,0.8)",
-	//             highlightFill: "rgba(220,220,220,0.75)",
-	//             highlightStroke: "rgba(220,220,220,1)",
-	//             data: [3, 1]
-	//         },
-	//         {
-	//             label: "My Second dataset",
-	//             fillColor: "rgba(151,187,205,0.5)",
-	//             strokeColor: "rgba(151,187,205,0.8)",
-	//             highlightFill: "rgba(151,187,205,0.75)",
-	//             highlightStroke: "rgba(151,187,205,1)",
-	//             data: [5, 2]
-	//         }
-	//     ]
-	// };
-
-	// var options = {
-	//     //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-	//     scaleBeginAtZero : true,
-
-	//     //Boolean - Whether grid lines are shown across the chart
-	//     scaleShowGridLines : true,
-
-	//     //String - Colour of the grid lines
-	//     scaleGridLineColor : "rgba(0,0,0,.05)",
-
-	//     //Number - Width of the grid lines
-	//     scaleGridLineWidth : 1,
-
-	//     //Boolean - Whether to show horizontal lines (except X axis)
-	//     scaleShowHorizontalLines: true,
-
-	//     //Boolean - Whether to show vertical lines (except Y axis)
-	//     scaleShowVerticalLines: true,
-
-	//     //Boolean - If there is a stroke on each bar
-	//     barShowStroke : true,
-
-	//     //Number - Pixel width of the bar stroke
-	//     barStrokeWidth : 2,
-
-	//     //Number - Spacing between each of the X value sets
-	//     barValueSpacing : 5,
-
-	//     //Number - Spacing between data sets within X values
-	//     barDatasetSpacing : 1,
-
-	//     responsive: true, 
-
-	//     maintainAspectRatio: true,
-
-	//     //String - A legend template
-	//     legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-
-	// };
-
+	
 	var inView = false;
 
-	// function isScrolledIntoView(elem)
-	// {
-	//     var docViewTop = $(window).scrollTop();
-	//     var docViewBottom = docViewTop + $(window).height();
+	// jQuery.expr.filters.offscreen = function(el) {
+	//   return (
+	//               (el.offsetLeft + el.offsetWidth) < 0 
+	//               || (el.offsetTop + el.offsetHeight) < 0
+	//               || (el.offsetLeft > window.innerWidth || el.offsetTop > window.innerHeight)
+	//          );
+	// };
 
-	//     var elemTop = $(elem).offset().top;
-	//     var elemBottom = elemTop + $(elem).height();
-
-	//     return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
-	// }
-
-	// $(window).scroll(function() {
-	//     if (isScrolledIntoView('#chart1')) {
-	//         if (inView) { return; }
-	//         inView = true;
-	//         setTimeout(function(){
-	        	
-	//         }, 1000);
-	        
-	//     } else {
-	//         inView = false;  
-	//     }
-	// });
-
-	new Chartist.Bar('#chart1', {
-	  labels: ['1000 mg of L-canitine', '1000mg of other'],
-	  series: [
-	    [
-	    	{meta: 'Max', value: 3},
-	    	{meta: 'Min', value: 1}
-	    ],
-	    [
-	    	{meta: 'Max', value: 5}, 
-	    	{meta: 'Min', value: 2}
-	    ]
-	  ]
-	}, {
-		seriesBarDistance: 12,
-		stackBars: true,
-		axisY: {
-			labelInterpolationFnc: function(value) {
-		    	return (value) + '';
-		    },
-		plugins: [
-		    Chartist.plugins.tooltip()
-		],
-	  }
-	}).on('draw', function(data) {
-	  if(data.type === 'bar') {
-	    data.element.attr({
-	      style: 'stroke-width: 30px'
-	    });
-	  }
+	$(window).scroll(function(){
+		if(isScrolledIntoView($('#chart1'))){
+			$('.chart1').fadeIn();
+			console.log("1");
+		}
+		if(isScrolledIntoView($('#chart2'))){
+			$('.chart2').fadeIn();
+			console.log("2");
+		}
+		if(isScrolledIntoView($('#chart3'))){
+			$('.chart3').fadeIn();
+			console.log("3");
+		}
+		console.log("Hello");
 	});
+
+	function isScrolledIntoView(elem)
+	{
+	    var $elem = $(elem);
+	    var $window = $(window);
+
+	    var docViewTop = $window.scrollTop();
+	    var docViewBottom = docViewTop + $window.height();
+
+	    var elemTop = $elem.offset().top;
+	    var elemBottom = elemTop + $elem.height();
+
+	    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	}
+
+	
+
+	// new Chartist.Bar('#chart1', {
+	//   labels: ['1000 mg of L-canitine', '1000mg of other'],
+	//   series: [
+	//     [
+	//     	{meta: 'Max', value: 3},
+	//     	{meta: 'Min', value: 1}
+	//     ],
+	//     [
+	//     	{meta: 'Max', value: 5}, 
+	//     	{meta: 'Min', value: 2}
+	//     ]
+	//   ]
+	// }, {
+	// 	seriesBarDistance: 12,
+	// 	stackBars: true,
+	// 	axisY: {
+	// 		labelInterpolationFnc: function(value) {
+	// 	    	return (value) + '';
+	// 	    },
+	// 	plugins: [
+	// 	    Chartist.plugins.tooltip()
+	// 	],
+	//   }
+	// }).on('draw', function(data) {
+	//   if(data.type === 'bar') {
+	//     data.element.attr({
+	//       style: 'stroke-width: 30px'
+	//     });
+	//   }
+	// });
 	// var ctx = document.getElementById("firstChart").getContext("2d");
 	// var myBarChart = new Chart(ctx).StackedBar(data, options);
 
