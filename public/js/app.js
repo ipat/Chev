ab335b3144905b75d9ea09c6c4dcd2775ad9d905
Pat <pat.hornchaiya@hotmail.com>
2016-08-10 21:30:07 +0700

    Add new products

--- public/js/app.js
+++ public/js/app.js
@@ -626,8 +626,8 @@ chevApp.controller('signupController', function($scope, $rootScope, Users){
 
 chevApp.controller('productsController', function($scope, $rootScope, $resource, Cart){
 	$rootScope.navbarClass = "text-dark";
-	$scope.amount1 = 1;
-	$scope.amount4 = 1;
+	$scope.amount2 = 1;
+	$scope.amount3 = 1;
 	
 	$(".show-more a").on("click", function() {
 	    var $this = $(this); 
